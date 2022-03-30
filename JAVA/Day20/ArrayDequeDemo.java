b20b5c24df606b0bd93e2c2753f8f4f79944f8f2
ANN MARIA JENSON <61185912+ANNMARIAJENSON@users.noreply.github.com>
2022-03-30 17:42:06 +0530

    Add files via upload

---
+++ JAVA/Day20/ArrayDequeDemo.java
@@ -0,0 +1,18 @@
+package Day20;
+
+import java.util.*;
+public class ArrayDequeDemo {
+    public static void main(String[] args)
+    {
+        // Initializing an deque
+        Deque<String> dq
+            = new ArrayDeque<String>();
+  
+        // add() method to insert
+        dq.add("To");
+        dq.addFirst("Welcome");
+        dq.addLast("Java");
+  
+        System.out.println(dq);
+    }
+}
