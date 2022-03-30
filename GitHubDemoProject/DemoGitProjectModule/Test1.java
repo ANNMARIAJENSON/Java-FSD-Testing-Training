0196001c4eaf9790197372ce065db45a8964140d
ANN MARIA JENSON <61185912+ANNMARIAJENSON@users.noreply.github.com>
2022-03-30 17:48:48 +0530

    Add files via upload

---
+++ GitHubDemoProject/DemoGitProjectModule/Test1.java
@@ -0,0 +1,17 @@
+package DemoGitProjectModule;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String string="YES";
+		if(string=="YES")
+		{
+			System.out.println("We can eat");
+		}
+		else
+		{
+			System.out.println("We cannot eat");
+		}
+	}
+
+}
