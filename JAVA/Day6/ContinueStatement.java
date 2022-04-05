0eb0b5c1d81ed8cf544d9cdba734df627dd69470
ANN MARIA JENSON <61185912+ANNMARIAJENSON@users.noreply.github.com>
2022-04-05 09:58:45 +0530

    Add files via upload

---
+++ JAVA/Day6/ContinueStatement.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class ContinueStatement {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			if(i==5)
+			{
+				continue;
+			}
+			System.out.print(" "+i);
+		}
+		System.out.println("\n"+"End of for loop");
+	}
+
+}
